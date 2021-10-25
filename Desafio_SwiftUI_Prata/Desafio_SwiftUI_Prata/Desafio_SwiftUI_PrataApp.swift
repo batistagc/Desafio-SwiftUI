d2edc695c1b7ad2dffb3897c290ceeb6576e79c9
Gabriel Batista <gabriel.devproject@gmail.com>
2021-10-25 16:32:52 -0300

    Add files via upload

---
+++ Desafio_SwiftUI_Prata/Desafio_SwiftUI_Prata/Desafio_SwiftUI_PrataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Desafio_SwiftUI_PrataApp.swift
+//  Desafio_SwiftUI_Prata
+//
+//  Created by Gabriel Batista Cristiano on 25/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Desafio_SwiftUI_PrataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
